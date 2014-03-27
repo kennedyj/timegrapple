36ce6d599b8d07df04ef0a8f38eee50e0bda470f
Josh Kennedy <kennedy.josh@gmail.com>
2014-03-27 16:01:30 -0700

    adding apaches

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os.path
+from werkzeug.serving import run_simple
 
 # TODO: config
 _ROOT = os.path.abspath(os.path.dirname(__file__))
@@ -8,4 +9,8 @@ import timegrapple.data
 import timegrapple.views
 
 if __name__ == '__main__':
-    timegrapple.app.app.run()
+    host = "127.0.0.1"
+    port = 5100
+    run_simple(
+        host, port, timegrapple.app.app, threaded=True,
+        use_reloader=True, use_debugger=True, use_evalex=True)
